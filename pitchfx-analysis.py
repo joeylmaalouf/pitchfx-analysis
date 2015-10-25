83bf9c6c3df6156923ca61cbe64a1a681f63c12e
joeylmaalouf <joeylmaalouf@gmail.com>
2015-10-25 14:59:32 -0400

    basic tree element parsing for pitch type

--- pitchfx-analysis.py
+++ pitchfx-analysis.py
@@ -1,8 +1,10 @@
 import os
 import sys
+from xml.etree import ElementTree as ET
 
 
 def main(argv):
+  # if no data path is supplied, use our default
   data_path = "./data/" if len(argv) < 2 else argv[1]
 
   # walk through the data path and make our list of inning files
@@ -10,7 +12,28 @@ def main(argv):
   for root, dirnames, filenames in os.walk(data_path):
     for filename in filenames:
       if "inning" in filename.lower():
-        data_filenames.append(filename)
+        data_filenames.append(root + "/" + filename)
+
+  # parse our .xml files via an element tree
+  pitch_types = []
+  for filename in data_filenames:
+    try:
+      game = ET.parse(filename)
+    except ET.ParseError:
+      # if the file can't be parsed, skip it
+      continue
+
+    # for every game, iterate through the elements to find the pitch types
+    for inning in game.getroot():
+      for half in inning:
+        for event in half:
+          if event.tag == "atbat":
+            for action in event:
+              if action.tag == "pitch":
+                pitch = action.get("pitch_type", None)
+                if pitch:
+                  pitch_types.append(pitch)
+  print(len(pitch_types))
 
 
 if __name__ == "__main__":
