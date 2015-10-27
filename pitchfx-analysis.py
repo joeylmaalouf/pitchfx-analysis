0a99fe4cf2b90eb3b863da392b9a851d4db51b55
joeylmaalouf <joeylmaalouf@gmail.com>
2015-10-26 23:18:01 -0400

    added parsing for handedness as well as the existing pitch sequence; functionized chart generation

--- pitchfx-analysis.py
+++ pitchfx-analysis.py
@@ -4,6 +4,40 @@ import sys
 from lxml import etree as ET
 
 
+def bar_charts(results, title, width = 0.2):
+  """ Given a results dictionary, plot the varying
+  outcome classes side-by-side on a bar chart. """
+  plt.figure()
+  plt.title(title, fontweight = "bold")
+  for i, (k, v) in enumerate(results.items()):
+    plt.bar(i - 3 * width / 2, v["hit"],       width = width, align = "center", color = "r")
+    plt.bar(i -     width / 2, v["hitout"],    width = width, align = "center", color = "g")
+    plt.bar(i +     width / 2, v["strikeout"], width = width, align = "center", color = "b")
+    plt.bar(i + 3 * width / 2, v["walk"],      width = width, align = "center", color = "k")
+  plt.xticks(range(len(results)), [", ".join(throws) for throws in results.keys()], rotation = 30)
+  plt.legend(["hit", "hitout", "strikeout", "walk"], fancybox = True, shadow = True)
+  plt.tight_layout()
+  plt.show()
+
+
+def pie_charts(ratios, title):
+  """ Given a results dictionary, plot the ratios of
+  the varying outcome classes on a pie chart. """
+  fig, axes = plt.subplots(2, 2)
+  axes = axes.reshape(4)
+  fig.suptitle(title, fontweight = "bold")
+  colors = ["r", "g", "b", "k"]
+  for i, (k, v) in enumerate(ratios.items()):
+    axes[i].pie([v["hit"], v["hitout"], v["strikeout"], v["walk"]],
+                labels = ["hit", "hitout", "strikeout", "walk"],
+                labeldistance = 1.1, colors = colors,
+                shadow = True, counterclock = False)
+    axes[i].axis("equal")
+    axes[i].set_xlabel(", ".join(k))
+  plt.tight_layout(pad = 3.0)
+  plt.show()
+
+
 def main(argv):
   # if no data path is supplied, use our default
   data_path = "./data/" if len(argv) < 2 else argv[1]
@@ -31,11 +65,18 @@ def main(argv):
   }
 
   # the results dictionary, listing the number of each type of events each sequence leads to
-  results = {
+  sequence_results = {
     ("fast", "fast", "curve"):  { "hit": 0, "hitout": 0, "strikeout": 0, "walk": 0 },
     ("fast", "fast", "fast"):   { "hit": 0, "hitout": 0, "strikeout": 0, "walk": 0 },
     ("fast", "slide", "slide"): { "hit": 0, "hitout": 0, "strikeout": 0, "walk": 0 },
-    ("slide", "fast", "slide"): { "hit": 0, "hitout": 0, "strikeout": 0, "walk": 0 }
+    ("slide", "fast", "slide"): { "hit": 0, "hitout": 0, "strikeout": 0, "walk": 0 },
+  }
+  # bonus results dict for the extra handedness graphs
+  handedness_results = {
+    ("L pitcher", "L batter"): { "hit": 0, "hitout": 0, "strikeout": 0, "walk": 0 },
+    ("L pitcher", "R batter"): { "hit": 0, "hitout": 0, "strikeout": 0, "walk": 0 },
+    ("R pitcher", "L batter"): { "hit": 0, "hitout": 0, "strikeout": 0, "walk": 0 },
+    ("R pitcher", "R batter"): { "hit": 0, "hitout": 0, "strikeout": 0, "walk": 0 }
   }
 
   # parse our .xml files via an element tree
@@ -55,44 +96,31 @@ def main(argv):
                             for action in event
                             if action.tag == "pitch" and "pitch_type" in action.attrib)
             sequence = pitches[-3:]
-            if sequence in results.keys():
+            if sequence in sequence_results.keys():
               outcome = outcome_classes[event.attrib["event"]]
-              results[sequence][outcome] += 1
+              sequence_results[sequence][outcome] += 1
+              handedness = (event.attrib["p_throws"] + " pitcher", event.attrib["stand"] + " batter")
+              handedness_results[handedness][outcome] += 1
 
-  # create a side-by-side absolute bar chart to compare the results
-  plt.figure()
-  plt.title("Absolute Comparison", fontweight = "bold")
-  for i, (k, v) in enumerate(results.items()):
-    plt.bar(i - 0.3, v["hit"],       width = 0.2, align = "center", color = "r")
-    plt.bar(i - 0.1, v["hitout"],    width = 0.2, align = "center", color = "g")
-    plt.bar(i + 0.1, v["strikeout"], width = 0.2, align = "center", color = "b")
-    plt.bar(i + 0.3, v["walk"],      width = 0.2, align = "center", color = "k")
-  plt.xticks(range(len(results)), [", ".join(throws) for throws in results.keys()], rotation = 30)
-  plt.legend(["hit", "hitout", "strikeout", "walk"], fancybox = True, shadow = True)
-  plt.tight_layout()
-  plt.show()
+  # create some bar charts to compare the results
+  bar_charts(sequence_results, "Absolute Comparison (Pitching Sequence)")
+  bar_charts(handedness_results, "Absolute Comparison (Handedness)")
 
   # turn the raw results into ratios
-  ratios = { sequence:
+  sequence_ratios = { sequence:
              { key:
                100.0 * val / sum(counts.values())
-              for key, val in results[sequence].items() }
-            for sequence, counts in results.items() }
+              for key, val in sequence_results[sequence].items() }
+            for sequence, counts in sequence_results.items() }
+  handedness_ratios = { handedness:
+             { key:
+               100.0 * val / sum(counts.values())
+              for key, val in handedness_results[handedness].items() }
+            for handedness, counts in handedness_results.items() }
 
-  # create a pie chart to compare the results
-  fig, axes = plt.subplots(2, 2)
-  axes = axes.reshape(4)
-  fig.suptitle("Percentage Comparison", fontweight = "bold")
-  colors = ["r", "g", "b", "k"]
-  for i, (k, v) in enumerate(ratios.items()):
-    axes[i].pie([v["hit"], v["hitout"], v["strikeout"], v["walk"]],
-                labels = ["hit", "hitout", "strikeout", "walk"],
-                labeldistance = 1.1, colors = colors,
-                shadow = True, counterclock = False)
-    axes[i].axis("equal")
-    axes[i].set_xlabel(", ".join(k))
-  plt.tight_layout(pad = 3.0)
-  plt.show()
+  # create some pie charts to compare the ratios
+  pie_charts(sequence_ratios, "Percentage Comparison (Pitching Sequence)")
+  pie_charts(handedness_ratios, "Percentage Comparison (Handedness)")
 
 
 if __name__ == "__main__":
